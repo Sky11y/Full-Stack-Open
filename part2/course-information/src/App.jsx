4b8ef43d891b586a9e5d225d878da5bb3e0bb679
Sky11y <jyri.piensalo@gmail.com>
2026-02-01 17:13:16 +0200

    Commit for finished exercise 2.5

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -1,48 +1,4 @@
-const Header = ({name}) => {
-	return (
-		<div>
-			<h1>{name}</h1>
-		</div>
-	) 
-}
-
-const Part = ({name, exercises}) => {
-	return (
-		<div>
-			<p>{name} {exercises}</p>
-		</div>
-	)
-}
-
-const Content = ({parts}) => {
-	return (
-		<div>
-			{parts.map(part =>
-				<Part key={part.id} name={part.name} exercises={part.exercises} />
-			)}
-		</div>
-	)
-}
-
-const Total = ({parts}) => {
-	const total = parts.reduce((total, part) => {
-		return total + part.exercises
-	}, 0)
-
-	return (
-		<div><strong>total of {total} exercises</strong></div>
-	)
-}
-
-const Course = ({course}) => {
-	return (
-		<div>
-			<Header name={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</div>
-	)
-}
+import Course from './Course'
 
 const App = () => {
 	const courses = [
