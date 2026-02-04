a95420aa8291b5faabf14514e571a3ed6fdeb609
Sky11y <jyri.piensalo@gmail.com>
2026-02-04 09:18:37 +0200

    Commit for finished exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,43 +1,51 @@
 import { useState } from 'react'
 
-const Content = ({person}) => {
+const Content = ({person, number}) => {
 	return (
-		<div>{person}</div>
+		<div>{person} {number}</div>
 	)
 }
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 	const addName = (event) => {
 		event.preventDefault()
 		if (persons.some(person => person.name === newName)) {
 			window.alert(`${newName} is already in the phonebook`)
+		} else if (persons.some(person => person.number === newNumber)) {
+			window.alert(`Phonenumber ${newNumber} is already in use`)
 		} else {
 			const newPerson = {
 				name: newName,
+				number: newNumber,
 				id: String(persons.length + 1),
 			}
 			setPersons(persons.concat(newPerson))
 			setNewName('')
+			setNewNumber('')
 		}
 	}
 
-	const handleFormChange = (event) => {
+	const handleNameChange = (event) => {
 		setNewName(event.target.value)
 	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
+	}
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input
-						value={newName}
-						onChange={handleFormChange} />
+          name: <input value={newName}	onChange={handleNameChange} />
         </div>
 				<div>
-					debug: {newName}	
+					number: <input value={newNumber} onChange={handleNumberChange} />
 				</div>
         <div>
           <button type="submit">add</button>
@@ -45,7 +53,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 			<div>
-				{persons.map(person => <Content key={person.id} person={person.name} />)}
+				{persons.map(person =>
+					<Content key={person.id} person={person.name} number={person.number} />)}
 			</div>
     </div>
   )
