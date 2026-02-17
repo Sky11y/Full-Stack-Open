1ffc5c06d755a589518739e9eae14cbcf12370c6
Sky11y <jyri.piensalo@gmail.com>
2026-02-17 15:24:05 +0200

    add put to modify existing persons number

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -68,6 +68,24 @@ app.post('/api/persons', (request, response) => {
 	})
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
+
+	Person.findById(request.params.id)
+		.then(person => {
+			if (!person) {
+				return response.status(404).end()
+			}
+
+			person.number = body.number
+
+			return person.save().then(updatedPerson => {
+				response.json(updatedPerson)
+			})
+		})
+		.catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint'})
 }
