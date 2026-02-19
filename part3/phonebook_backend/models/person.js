269644c73c55d8719e4acec31bff709b21c94a13
Sky11y <jyri.piensalo@gmail.com>
2026-02-19 10:52:45 +0200

    uibuild

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -4,48 +4,50 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-	.then(result => {
-		console.log('connected to MongoDB')
-	})
-	.catch(error => {
-		console.error('error connecting to MongoDB:', error.message)
-	})
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		validate: {
-			validator: function(v) {
-				return v.length >= 3;
-			},
-			message: props => `${props.value} is less than 3 characters long!`
-		},
-		required: true
-	},
-	number: {
-		type: String,
-		validate: {
-			validator: function(v) {
-				return v.length >= 8;
-			},
-			message: props => `${props.value} is too short. Minimum length is 8.`
-		},
-		validate: {
-			validator: function(v) {
-				return /\d{2,3}-\d{6,8}/.test(v);
-			},
-			message: props => `${props.value} is not a valid phone number!`
-		},
-		required: true
-	}
+  name: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return v.length >= 3
+      },
+      message: props => `${props.value} is less than 3 characters long!`
+    },
+    required: true
+  },
+  number: {
+    type: String,
+    validate: [
+      {
+        validator: function(v) {
+          return v.length >= 8
+        },
+        message: props => `${props.value} is too short. Minimum length is 8.`
+      },
+      {
+        validator: function(v) {
+          return /\d{2,3}-\d{6,8}/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    ],
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-	transform: (document, returnedObj) => {
-		returnedObj.id = returnedObj._id.toString()
-		delete returnedObj._id
-		delete returnedObj.__v
-	}
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
